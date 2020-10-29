c7c2f07b79eb58face1ce78171ca47d27da7b899
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-10-29 12:37:29 +0200

    App created.

---
+++ app/src/main/java/com/kagan/control_your_home/RoomsActivity.kt
@@ -0,0 +1,11 @@
+package com.kagan.control_your_home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoomsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rooms)
+    }
+}

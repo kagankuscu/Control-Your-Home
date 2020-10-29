133275e7a53dffec2e5f00f9d7edbaa84babfba5
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-10-29 17:08:21 +0200

    #2 Login UI created. Navigation graph added.

--- app/src/main/java/com/kagan/control_your_home/ui/MainActivity.kt
+++ app/src/main/java/com/kagan/control_your_home/ui/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.kagan.control_your_home
+package com.kagan.control_your_home.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kagan.control_your_home.R
 
-class RoomsActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rooms)
+        setContentView(R.layout.activity_main)
     }
 }
